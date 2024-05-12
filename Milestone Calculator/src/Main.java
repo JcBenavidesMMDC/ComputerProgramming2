b40fe05a2dd022400793d4715f212f4ef49992e2
JcBenavidesMMDC <163724625+JcBenavidesMMDC@users.noreply.github.com>
2024-05-13 04:56:44 +0800

    Delete Milestone Calculator directory

--- Milestone Calculator/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MilestoneCalculator program = new MilestoneCalculator();
-        program.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
