da4468629368937052483c8f1ff4d248e5234f20
JcBenavidesMMDC <163724625+JcBenavidesMMDC@users.noreply.github.com>
2024-05-19 03:04:46 +0800

    Update Main.java

--- Milestone 1/src/Main.java
+++ Milestone 1/src/Main.java
@@ -5,8 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	Login login = new Login();
         login.setVisible(true);
-
-	WorkTimeCalculator wtc = new WorkTimeCalculator(); 
-        wtc.setVisible(true);
     }
 }
