b7ae41b043024d65ea2721f172d3a8b82a0f7c4f
JcBenavidesMMDC <163724625+JcBenavidesMMDC@users.noreply.github.com>
2024-06-07 01:22:48 +0800

    Add files via upload

---
+++ Milestone 1/src/EmployeeData.java
@@ -0,0 +1,15 @@
+public class EmployeeData {
+    String[] empId = {"123", "234", "345"};
+    String[] empLname = {"Garcia", "Lim", "Aquino"};
+    String[] empFname = {"Manuel III", "Antonio", "Bianca Sofia"};
+    String[] empBday = {"10/11/1983", "06/19/1988", "08/04/1989"};
+    String[] empAddr = {"Valero Carpark Building Valero Street 1227, Makati City", "San Antonio De Padua 2, Block 1 Lot 8 and 2, Dasmarinas, Cavite", "Rm. 402 4/F Jiao Building Timog Avenue Cor. Quezon Avenue 1100, Quezon City"};
+    String[] empPhNo = {"966-860-270", "171-867-411", "966-889-370"};
+    String[] empSss = {"44-4506057-3", "52-2061274-9", "30-8870406-2"};
+    String[] empPhHealth = {"820126853951", "331735646338", "177451189665"};
+    String[] empTin = {"442-605-657-000", "683-102-776-000", "971-711-280-000"};
+    String[] empPagIbig = {"691295330870", "663904995411", "171519773969"};
+    double[] empSalary = {90000, 60000, 60000};
+    double[] empSemiMonthlyRate = {45000, 30000, 30000}; //empSalary divided by 2
+    double[] empHourlyRate = {535.71, 357.14, 357.14};
+}
